555f819b160e50f893cc93ff3326586d83c35079
vasiasan <vaasya@gmail.com>
2011-10-07 23:54:40 +0800

    Edited j/ga.js via GitHub

--- j/ga.js
+++ j/ga.js
@@ -1,1 +1,1 @@
-<script>_gaq=[['_setAccount','UA-7434570-3'],['_trackPageview']];(function(){var g=document.createElement('script');g.src='//www.google-analytics.com/ga.js';g.setAttribute('async','true');document.documentElement.firstChild.appendChild(g);})();</script>
+<script>_gaq=[['_setAccount','UA-26177087-1'],['_trackPageview']];(function(){var g=document.createElement('script');g.src='//www.google-analytics.com/ga.js';g.setAttribute('async','true');document.documentElement.firstChild.appendChild(g);})();</script>
